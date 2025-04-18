8a8c1f4e0df84a8fd45cc3ae916a28ac987b478c
Y.Nanthachai <thecoachingonline@gmail.com>
2025-04-18 02:49:10 +0000

    Host:3000

--- server.js
+++ server.js
@@ -1,13 +1,3 @@
-var express = require('express');
-var app = express();
-var helloWorld = function(req, res, next) {
-    res.setHeader('content-type', 'text/plain');
-    res.end('Hello World');
-};
+var express = require('./bin/www');
 
-app.use('/', helloWorld);
-
-app.listen(3000);
 console.log('Sever running at http://localhost:3000');
-
-module.express = app;
