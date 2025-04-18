42e33cc73ab2511e9cce9e71e1b4e01e6be90829
Y.Nanthachai <thecoachingonline@gmail.com>
2025-04-18 06:16:15 +0000

    Sass

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var sass = request('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,6 +18,12 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(sass({
+  src: './sass',
+  dest: './public/stylesheets',
+  outputStyle: 'compressed',
+  prefix: 'stylesheets'
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
