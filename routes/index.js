42d7b4db41fa74e2e74ff11a9091bb170ca0c17a
Y.Nanthachai <thecoachingonline@gmail.com>
2025-04-18 03:43:20 +0000

    Jade

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express 5' });
+  res.render('index', { title: 'Express 5', message: 'How are things' });
 });
 /* POST home page. */
 router.post('/', function(req, res, next) {
