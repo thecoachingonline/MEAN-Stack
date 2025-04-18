7a5c4a8241aa331c80e79362da927f1006ccf0e1
Y.Nanthachai <thecoachingonline@gmail.com>
2025-04-18 02:40:20 +0000

    Request Object

--- routes/users.js
+++ routes/users.js
@@ -5,5 +5,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+/* Put */
+router.put('/user', function(req, res, next) {
+  res.send('Got a PUT request at /user')
+})
+/* Delete */
+router.delete('/user', function(req, res, next) {
+  res.send('Got a DELETE request at /user')
+})
 
 module.exports = router;
