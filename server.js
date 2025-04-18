65715b1d3f3f8867fe1823c3794c5c3d3c558fb5
Y.Nanthachai <thecoachingonline@gmail.com>
2025-04-18 03:16:16 +0000

    Enviroment

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var express = require('./bin/www');
 
+module.exports = express;
 console.log('Sever running at http://localhost:3000');
